558bda643d93c0f7297e6c90911fc74ea2401b97
ALBERT <thatguyintech@gmail.com>
2022-02-28 23:31:51 -0800

    wrap App in wagmi Provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Outlet, Link } from "react-router-dom";
+import { Provider } from "wagmi";
 
 export default function App() {
   return (
-    <div>
-      <h1>HACKRS</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/mint">mint</Link> - {" "}
-        <Link to="/nfts">view my HACKRs</Link>
-      </nav>
-      <Outlet />
-    </div>
+    <Provider>
+      <div>
+        <h1>HACKRS</h1>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <Link to="/mint">mint</Link> - {" "}
+          <Link to="/nfts">view my HACKRs</Link>
+        </nav>
+        <Outlet />
+      </div>
+    </Provider>
   );
 }

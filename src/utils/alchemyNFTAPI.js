b7161d2dc2d760033740257a90854a2b2b4c746a
ALBERT <thatguyintech@gmail.com>
2022-03-01 19:15:29 -0800

    filter getNFTs to only HACKR tokens

--- src/utils/alchemyNFTAPI.js
+++ src/utils/alchemyNFTAPI.js
@@ -5,11 +5,12 @@ export async function getNFTs(ownerAddr) {
     // Replace with your Alchemy API key:
     const apiKey = "jgXYGxWMAYsxaym5fgNqR1Mvx4ozZco7";
     const baseURL = `https://eth-mainnet.g.alchemy.com/${apiKey}/v1/getNFTs`;
+    const contractAddr = "0x3Eb0Debc2F1FDb613416B3718670f64415C5fF16";
 
     // Setup request options:
     var config= {
         method: 'get',
-        url: `${baseURL}?owner=${ownerAddr}`
+        url: `${baseURL}?owner=${ownerAddr}&contractAddresses[]=${contractAddr}`
     };
 
     let resp;
